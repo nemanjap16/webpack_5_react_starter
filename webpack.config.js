6bb2005c2fae5d51a3d04bf009edd7376cbc9b13
NemanjaP83 <necaintruder@gmail.com>
2022-03-22 17:16:13 +0100

    style loaders

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    app: path.resolve(__dirname, "src/index.js"),
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+};
